703f3c06029d8eef03161b8f1ae1d72b99066628
SaiKUndurthi <sai.undurthi@gmail.com>
2018-02-02 19:09:41 -0800

    Forms and Encapsulation for battle.

--- github_battle/src/components/App.js
+++ github_battle/src/components/App.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 import '../index.css';
 import Popular from './Popular.js';
+import Home from './Home.js';
+import Battle from './Battle.js';
+import Nav from './Nav.js';
+import Results from './Results.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <Popular />
-      </div>
+      <Router>
+        <div className="container">
+          <Nav />
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path="/popular" component={Popular} />
+              <Route path="/battle/results" component={Results} />
+              <Route exact path='/battle' component={Battle} />
+              <Route render={function(){
+                return(
+                  <p> Not Found </p>
+                )
+              }} />
+            </Switch>
+        </div>
+      </Router>
     );
   }
 }
